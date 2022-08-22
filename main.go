bc40dc9fc70f02fc42ee334cc107460fa18a8970
Avraham Plotnitzky <Avraham.Plotnitzky@amdocs.com>
2022-08-22 13:02:39 +0300

    main

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 
 import (
 	"github.com/aviplot/go-finance-math/financial"
-	"github.com/shopspring/decimal"
 	"github.com/aviplot/go-mortgage-calc/mortgage"
+	"github.com/shopspring/decimal"
+
 )
+
 func main() {
 	d := financial.NewDateFromFormattedString("2020-05-20")
 	a := decimal.NewFromFloat(1.23)
